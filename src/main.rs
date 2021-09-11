01b7c394bdd405779d2c4e2ed788748c641f38e8
Brendan Maclean <brendan.maclean@alumni.ubc.ca>
2021-09-11 15:42:14 -0600

    Get league selection using dialoguer dependency. Adds that dep.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use dialoguer::Select;
+use std::process;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!("Time to build your fantasy schedule. We need some information to get you started.");
+
+    let league_options = vec!["Sleeper", "NFL.com", "ESPN"];
+    let league_index = Select::new().with_prompt("Select your fantasy platform")
+        .items(&league_options)
+        .default(0)
+        .interact()?;
+    let league_selection = league_options[league_index];
+
+    if league_selection != "Sleeper" {
+        println!("This CLI currently only supports Sleeper leagues.");
+        process::exit(1);
+    }
+
+    println!("{}", league_selection);
+
+    Ok(())
 }
